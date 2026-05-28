aac12766fa2e13fcbe05ef7093c4fdff94bfd91e
emmanuel123 <obaloluwaajayi2006@gmail.com>
2026-05-27 23:34:31 -0700

    first benedex frontend commit!

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { BrowserRouter } from "react-router-dom";
+
+function App(){
+
+return(
+
+<BrowserRouter>
+
+<div>
+
+<h1>
+Benedex LMS
+</h1>
+
+</div>
+
+</BrowserRouter>
+
+);
+
+}
+
+export default App;
